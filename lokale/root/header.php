ee5a60ccae1b712731da9a9405c8f6562dde302d
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-11-14 13:04:07 +0100

    setup

--- lokale/root/header.php
+++ lokale/root/header.php
@@ -1,1 +1,21 @@
+<?php
+session_start(); //Starts session
 
+$servername = "db"; //Sets database info to variables for the sql connect
+$username = "root";
+$password = "Dn91ND1aMw3ZkuENdC1j1J8F";
+$database = "Lokaler";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?>
+<html lang="en" dir="ltr">
+  <head>
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
+  </head>
+</html>
