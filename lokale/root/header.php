0c631bc5f03bc56955a883845aa718b70d214ac3
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-12-05 12:31:35 +0100

    klargøring

--- lokale/root/header.php
+++ lokale/root/header.php
@@ -15,8 +15,3 @@ if (!$conn) {
 }
 
 ?>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.css">
-  </head>
-</html>
--- lokale/root/home.php
+++ lokale/root/home.php
@@ -7,20 +7,6 @@
     <title></title>
   </head>
   <body>
-    <div class="tile is-ancestor">
-      <div class="tile is-4 is-vertical is-parent">
-        <div class="tile is-child box">
-          <p class="title">Lokale Booker</p>
-        </div>
-        <div class="tile is-child box">
-          <p class="subtitle">Find <strong>dit</strong> frie lokale på skolen</p>
-        </div>
-      </div>
-      <div class="tile is-parent">
-        <div class="tile is-child box">
-          <p class="title">Registre & Login</p>
-        </div>
-      </div>
-    </div>
+  
   </body>
 </html>
