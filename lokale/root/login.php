41824d48c832652277c13cdcae422ae98ff5582a
flixhack <felix.benjamin.mathiassen@gmail.com>
2018-12-10 12:29:21 +0100

    log ind

--- lokale/root/login.php
+++ lokale/root/login.php
@@ -1,11 +1,10 @@
 <?php
 include '/Header.php';
 
-$email2 = $_POST['Email']; /*Sets a variable with the text from Email input*/
-$email = strtolower($email2); /*Sets the email to be all lowercase*/
+$Navn = $_POST['Navn']; /*Sets a variable with the text from Email input*/
 $preHashPass = $_POST['Password']; /*Sets a variable with the text from Password input*/
 $passwordLogin = hash('sha1', $preHashPass); /*Hashes the password*/
-$loginCheck="SELECT * FROM Bruger WHERE Navn = '{$email}' AND Password = '{$passwordLogin}' AND Klasse = '{$Klasse}'"; /*Making an sql-query that search for an matching email and hashed password*/
+$loginCheck="SELECT * FROM Bruger WHERE Navn = '{$Navn}' AND Password = '{$passwordLogin}' AND Klasse = '{$Klasse}'"; /*Making an sql-query that search for an matching email and hashed password*/
 
 $rs = mysqli_query($conn, $loginCheck); /*Connect to our database and runs the sql-query*/
 
