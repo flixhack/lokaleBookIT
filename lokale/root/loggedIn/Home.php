4f4842e1ccc50bc83a1c4ec8eebd130735eda595
flixhack <felix.benjamin.mathiassen@gmail.com>
2019-01-07 12:28:46 +0100

    iconbar

--- lokale/root/loggedIn/Home.php
+++ lokale/root/loggedIn/Home.php
@@ -3,3 +3,20 @@ include '../header.php';
 include 'checkSession.php';
 
  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="icon-bar">
+      <a href="Profilepage.php"><i class="fa fa-user"></i></a>
+      <a href="../ChatApp/#"><i class="fa fa-envelope"></i></a>
+      <a href="Settings.php"><i class="fa fa-gear"></i></a>
+      <a class="active" href="webCore.php"><i class="fa fa-home"></i></a>
+      <a href="../destroySession.php"><i class="fa fa-sign-out"></i></a>
+    </div>
+
+  </body>
+</html>
