18a70fcfeb677c353fefe1ba31ab6cde3e243f4a
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2019-01-14 11:04:38 +0100

    Ændring af menubar

--- lokale/root/loggedIn/Settings.php
+++ lokale/root/loggedIn/Settings.php
@@ -14,8 +14,8 @@ include 'HTMLIncluder.php';
 
     <div class="icon-bar">
       <a href="Home.php"><i class="fa fa-table"></i></a>
-      <a href=""><i class="fa fa-envelope"></i></a>
-      <a href=""><i class="fa fa-bell"></i></a>
+      <a href="chitchat/chat.php"><i class="fa fa-envelope"></i></a>
+      <a href="Notifications.php"><i class="fa fa-bell"></i></a>
       <a class="active" href="Settings.php"><i class="fa fa-gear"></i></a>
       <a href="../destroySession.php"><i class="fa fa-sign-out"></i></a>
     </div>
--- lokale/root/loggedIn/chitchat/chat.php
+++ lokale/root/loggedIn/chitchat/chat.php
@@ -3,3 +3,21 @@ include '../../header.php';
 include '../checkSession.php';
 
  ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+     <div class="icon-bar">
+       <a href="Home.php"><i class="fa fa-table"></i></a>
+       <a class="active" href="chitchat/chat.php"><i class="fa fa-envelope"></i></a>
+       <a href="Notifications.php"><i class="fa fa-bell"></i></a>
+       <a href="Settings.php"><i class="fa fa-gear"></i></a>
+       <a href="../destroySession.php"><i class="fa fa-sign-out"></i></a>
+     </div>
+
+   </body>
+ </html>
