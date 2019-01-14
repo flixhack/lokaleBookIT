3c415a65ad5bf82a554ad87eee86c96ad556d393
Magn2138 <44970047+Magn2138@users.noreply.github.com>
2019-01-14 13:12:26 +0100

    booties

---
+++ lokale/root/ikTænkPåDet/Front.php
@@ -0,0 +1,76 @@
+<?php
+include 'header.php';
+// include 'stylesheet.php';
+?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!-- <link href="style2.css" rel="stylesheet" type="text/css"> -->
+    <title>LokaleBooking - Aarhus Gymnasium</title>
+    <!-- <style>
+
+    a.format {
+        float: right;
+        color: #ffffff;
+        padding: 150px;
+}
+select {
+width: 100%;
+padding:3px 3px;
+border: 1px;
+border-radius: 0px;
+background-color: #ffffff;
+}
+    </style> -->
+
+    <link href="signin.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+<img src="logo2.png" alt="Title" style="width:660px;height:88px;">
+
+</script>
+  </body>
+<form name="login" action="login.php" method="POST">
+  <input type="text" style="position:absolute;TOP:35px;left:670px" placeholder="Fornavn" name="Navn" id="Navn"><br>
+  <input type="text" style="position:absolute;TOP:35px;left:850px" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
+  <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Kodeord" name="Password" id="Password"><br>
+  <!--<input type="select" style="position:absolute;TOP:35px;left:760px;WIDTH:70" name="klasse" id="Klasse">
+  <option value="16xaa">16xaa</option>
+  <option value="16xab">16xab</option>
+  <option value="16xac">16xac</option>
+  <option value="16xad">16xad</option>
+  <option value="16xae">16xae</option>
+  <option value="16xaf">16xaf</option>
+  <option value="16xaj">16xaj</option>
+  <option value="16xap">16xap</option>
+  <option value="16xar">16xar</option>
+  <option value="17xaa">17xaa</option>
+  <option value="17xab">17xab</option>
+  <option value="17xac">17xac</option>
+  <option value="17xad">17xad</option>
+  <option value="17xae">17xae</option>
+  <option value="17xaj">17xaj</option>
+  <option value="17xap">17xap</option>
+  <option value="17xaq">17xaq</option>
+  <option value="17xar">17xar</option>
+  <option value="18xaa">18xaa</option>
+  <option value="18xab">18xab</option>
+  <option value="18xad">18xad</option>
+  <option value="18xae">18xae</option>
+  <option value="18xaj">18xaj</option>
+  <option value="18xap">18xap</option>
+  <option value="18xaq">18xaq</option>
+  <option value="18xar">18xar</option>
+</select> -->
+  <input type="submit" value="Login" style="position:absolute;TOP:35px;left:1210px;WIDTH:60;HEIGHT:22">
+</form>
+
+  <A HREF="signUp/signUpSite.php" class="format">Opret bruger</A>
+
+  <p class="sansserif">Velkommen til lokalebooking på Aarhus Gymnasium. <br /> Denne webside er en service, der kan hjælpe dig <br /> med hurtigt at få et overblik over ledige lokaler på skolen <br /> samtidig med at give dig en mulighed  <br /> for at reservere det lokale du har brug for. </p>
+</html>
+
+
+ <!-- style="color:#ffffff;position:absolute;TOP:60px;left:850px" -->
