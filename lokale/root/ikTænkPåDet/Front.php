8ff500d02d9ffafa481ef00c26a2bdb4d1b4af68
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2019-01-16 14:09:43 +0100

    Rettelser i front, login virker stadig ikke

--- lokale/root/ikTænkPåDet/Front.php
+++ lokale/root/ikTænkPåDet/Front.php
@@ -36,34 +36,7 @@ background-color: #ffffff;
   <input type="text" style="position:absolute;TOP:35px;left:670px" placeholder="Fornavn" name="Navn" id="Navn"><br>
   <input type="text" style="position:absolute;TOP:35px;left:850px" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
   <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Kodeord" name="Password" id="Password"><br>
-  <!--<input type="select" style="position:absolute;TOP:35px;left:760px;WIDTH:70" name="klasse" id="Klasse">
-  <option value="16xaa">16xaa</option>
-  <option value="16xab">16xab</option>
-  <option value="16xac">16xac</option>
-  <option value="16xad">16xad</option>
-  <option value="16xae">16xae</option>
-  <option value="16xaf">16xaf</option>
-  <option value="16xaj">16xaj</option>
-  <option value="16xap">16xap</option>
-  <option value="16xar">16xar</option>
-  <option value="17xaa">17xaa</option>
-  <option value="17xab">17xab</option>
-  <option value="17xac">17xac</option>
-  <option value="17xad">17xad</option>
-  <option value="17xae">17xae</option>
-  <option value="17xaj">17xaj</option>
-  <option value="17xap">17xap</option>
-  <option value="17xaq">17xaq</option>
-  <option value="17xar">17xar</option>
-  <option value="18xaa">18xaa</option>
-  <option value="18xab">18xab</option>
-  <option value="18xad">18xad</option>
-  <option value="18xae">18xae</option>
-  <option value="18xaj">18xaj</option>
-  <option value="18xap">18xap</option>
-  <option value="18xaq">18xaq</option>
-  <option value="18xar">18xar</option>
-</select> -->
+
   <input type="submit" value="Login" style="position:absolute;TOP:35px;left:1210px;WIDTH:60;HEIGHT:22">
 </form>
 
