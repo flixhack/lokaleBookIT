425cdb41c8c0ee0ae52d9cecda5e43ebd75bc717
Magn2138 <44970047+Magn2138@users.noreply.github.com>
2019-01-16 13:39:33 +0100

    Fjernet "Opret bruger"

--- lokale/root/Front.php
+++ lokale/root/Front.php
@@ -40,8 +40,6 @@ background-color: #ffffff;
   <!--<input type="text" style="position:absolute;TOP:35px;left:670px" placeholder="Fornavn" name="Navn" id="Navn"><br>
   <input type="text" style="position:absolute;TOP:35px;left:850px" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
   <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Kodeord" name="Password" id="Password"><br>-->
-        <h1 class="h3 mb-3 font-weight-normal">Opret bruger</h1>
-
             <label for="inputFornavn" class="sr-only"/label>
             <input type="fornavn" id="inputFornavn" class="form-control" placeholder="Fornavn" required autofocus>
 
