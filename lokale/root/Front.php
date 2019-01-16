6c5621a41b3b8ecc489540aae1a4fdeda0f6ef00
Magn2138 <44970047+Magn2138@users.noreply.github.com>
2019-01-16 14:57:20 +0100

    fjernet opret bruger

--- lokale/root/Front.php
+++ lokale/root/Front.php
@@ -35,15 +35,20 @@ background-color: #ffffff;
 </script>
   <body class="text-right">
     <img src="logo2.png" alt="Title" style="width:660px;height:88px;">
-    <form name="login" action="login.php" method="POST">
-      <form class="form-signin text-center">
+      <form name="login" action="login.php" method="POST">
+      <form class="form-signin text-right">
   <!--<input type="text" style="position:absolute;TOP:35px;left:670px" placeholder="Fornavn" name="Navn" id="Navn"><br>
   <input type="text" style="position:absolute;TOP:35px;left:850px" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
   <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Kodeord" name="Password" id="Password"><br>-->
-        <h1 class="h3 mb-3 font-weight-normal text-center">Opret bruger</h1>
-            <input type="text" id="Fornavn" name="Fornavn" class="form-control text-center" placeholder="Fornavn" required autofocus>
-            <input type="text" id="Efternavn" name="Efternavn" class="form-control text-center" placeholder="Efternavn" required autofocus>
-            <input type="password" id="Password" name="Password" class="form-control text-center" placeholder="Kodeord" required autofocus>
+
+        <!-- <h1 class="h3 mb-3 font-weight-normal text-center">Opret bruger</h1> -->
+
+            <input type="text" id="Fornavn" class="form-control text-right" placeholder="Fornavn" required autofocus>
+
+            <input type="text" id="Efternavn" class="form-control text-right" placeholder="Efternavn" required autofocus>
+
+              <input type="password" id="Password" class="form-control text-right" placeholder="Kodeord" required autofocus>
+
               <div class="checkbox mb-3 text-center">
                 <label>
                   <input type="checkbox" value="husk-mig"> Husk mig
