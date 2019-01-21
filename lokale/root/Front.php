616b7132cfc9cfce802bddcda7b712227bc5b746
Magn2138 <44970047+Magn2138@users.noreply.github.com>
2019-01-21 12:35:52 +0100

    Absolute til fixed

--- lokale/root/Front.php
+++ lokale/root/Front.php
@@ -43,11 +43,11 @@ background-color: #ffffff;
 
         <!-- <h1 class="h3 mb-3 font-weight-normal text-center">Opret bruger</h1> -->
 
-            <input type="text" id="Fornavn" style="position:absolute;TOP:35px;left:670px" class="form-control text-right" placeholder="Fornavn" required autofocus>
+            <input type="text" id="Fornavn" style="position:fixed;TOP:35px;left:670px" class="form-control text-right" placeholder="Fornavn" required autofocus>
 
-            <input type="text" id="Efternavn" style="position:absolute;TOP:35px;left:850px" class="form-control text-right" placeholder="Efternavn" required autofocus>
+            <input type="text" id="Efternavn" style="position:fixed;TOP:35px;left:850px" class="form-control text-right" placeholder="Efternavn" required autofocus>
 
-            <input type="password" id="Password" style="position:absolute;TOP:35px;left:1030px" class="form-control text-right" placeholder="Kodeord" required autofocus>
+            <input type="password" id="Password" style="position:fixed;TOP:35px;left:1030px" class="form-control text-right" placeholder="Kodeord" required autofocus>
 
               <div class="checkbox mb-3 text-center">
                 <label>
