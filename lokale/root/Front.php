f08597b898763bb1926c5d2c214dedbd9eabc8ad
Martin Møldrup Jensen <37298117+HackerboyMartin@users.noreply.github.com>
2019-01-28 09:13:46 +0100

    Renaming front

--- lokale/root/Front.php
+++ lokale/root/Front.php
@@ -1,62 +1,82 @@
 <?php
 include 'header.php';
-include 'stylesheet.php';
-?>
 
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+  <!---<head>
     <meta charset="utf-8">
+<<<<<<< HEAD
+    <title>LokaleBooking - Aarhus Gymnasium</title>
+=======
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
-    <link href="style2.css" rel="stylesheet" type="text/css">
+    <link href="CSS.css" rel="stylesheet" type="text/css">
     <link href="/Programmering/loggedIn/style.css" rel="stylesheet" type="text/css">
-    <title>LokaleBooking - Aarhus Gymnasium</title>
-    <style>
+    <title>LokaleBooking - Aarhus Gymnasium</title>-->
 
-
-    </style>
-    <!-- <link href="style2.css" rel="stylesheet"> -->
+  <!-- <link href="style2.css" rel="stylesheet"> -->
+>>>>>>> c8a5037d93cd20d86097504f74aca551550ef171
   </head>
-<!-- </script> -->
-  <body class="text-right">
-    <img src="logo2.png" alt="Title" style="width:660px;height:88px;">
-    <form name="login" action="login.php" method="POST">
-      <form class="form-signin text-right">
-  <!--<input type="text" style="position:absolute;TOP:35px;left:670px" placeholder="Fornavn" name="Navn" id="Navn"><br>
-  <input type="text" style="position:absolute;TOP:35px;left:850px" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
-  <input type="password" style="position:absolute;TOP:35px;left:1030px" placeholder="Kodeord" name="Password" id="Password"><br>-->
-      <form action="/style2.css">
+
+<div class="container-1">
+  <div class="box-1">
+
+    <body class="text-right">
+      <link href="CSS.css" rel="stylesheet" type="text/css">
+      <img src="logo2.png" alt="Title" style="width:660px;height:88px;">
+      <form name="login" action="login.php" method="POST">
+        <form class="form-signin text-right">
+
+        </form>
+      </form>
+
+    </body>
+  </div>
+  <div class="box-2">
+
+  </div>
+  <div class="box-3">
+    <div class="Fornavn">
       <input type="text" name="Navn" id="Navn" style="position:sticky;" class="form-control" placeholder="Fornavn" required autofocus>
+    </div>
+    <div class="Efternavn">
       <input type="text" name="Efternavn" id="Efternavn" style="position:sticky;" class="form-control" placeholder="Efternavn" required autofocus>
+    </div>
+    <div class="Password">
       <input type="password" name="Password" id="Password" style="position:sticky;" class="form-control" placeholder="Kodeord" required autofocus>
-
-            <!-- <input type="text" id="Fornavn" style="position:sticky;" class="form-control" placeholder="Fornavn" required autofocus> -->
-
-            <!-- <input type="text" id="Efternavn" style="position:sticky;" class="form-control" placeholder="Efternavn" required autofocus> -->
-
-            <!-- <input type="password" id="Password" style="position:sticky;" class="form-control" placeholder="Kodeord" required autofocus> -->
-
-        <div style="position:sticky;" class="checkbox mb-3 text-center">
+    </div>
+    <div class="Opret">
+      <A HREF="signUp/signUpSite.php" class="format text-text">Opret bruger</A>
+    </div>
+  </div>
+  <div class="box-5">
+      <div class="Husk-mig">
+        <div class="checkbox mb-3 text-center">
           <label>
-            <input type="checkbox" style="position:sticky;" value="husk-mig"> Husk mig
+            <input type="checkbox" value="Husk-mig"> Husk mig
           </label>
         </div>
-        <input type="submit" style="position:sticky;" value="login">
-
-    <!-- <input type="submit" value="Login" style="position:absolute;TOP:35px;left:1210px;WIDTH:60;HEIGHT:22"> -->
-    </form>
-  </form>
-</body>
-
-  <A HREF="signUp/signUpSite.php" class="format text-text">Opret bruger</A>
+      </div>
+    <div class="Login">
+      <input type="submit" value="Login" text="Login">
+    </div>
+   </div>
+</div>
 
 
-  <p class="sansserif">Velkommen til lokalebooking på Aarhus Gymnasium. <br /> Denne webside er en service, der kan hjælpe dig <br /> med hurtigt at få et overblik over ledige lokaler på skolen <br /> samtidig med at give dig en mulighed  <br /> for at reservere det lokale du har brug for. </p>
+
+
+
+
+
+<div class="container-2">
+  <div class="box-4">
+    <p class="sansserif">Velkommen til lokalebooking på Aarhus Gymnasium. <br /> Denne webside er en service, der kan hjælpe dig <br /> med hurtigt at få et overblik over ledige lokaler på skolen <br /> samtidig med at give dig en mulighed <br /> for at reservere det lokale
+      du har brug for. </p>
+  </div>
+</div>
 </html>
-
-
- <!-- style="color:#ffffff;position:absolute;TOP:60px;left:850px" -->
