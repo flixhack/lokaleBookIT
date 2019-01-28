c8a5037d93cd20d86097504f74aca551550ef171
Magn2138 <44970047+Magn2138@users.noreply.github.com>
2019-01-28 09:06:32 +0100

    æløkjh

--- lokale/root/HTML.php
+++ lokale/root/HTML.php
@@ -5,13 +5,16 @@ include 'stylesheet.php';
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
+  <!---<head>
     <meta charset="utf-8">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
     <link href="CSS.css" rel="stylesheet" type="text/css">
     <link href="/Programmering/loggedIn/style.css" rel="stylesheet" type="text/css">
-    <title>LokaleBooking - Aarhus Gymnasium</title>
-    
+    <title>LokaleBooking - Aarhus Gymnasium</title>-->
+
   <!-- <link href="style2.css" rel="stylesheet"> -->
   </head>
 
