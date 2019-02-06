873ac6255d6607edd756cb313d18a754e4072796
Hyneth <45035426+Hyneth@users.noreply.github.com>
2019-02-06 13:51:10 +0100

    Test

--- lokale/root/signUp/signUpSite.php
+++ lokale/root/signUp/signUpSite.php
@@ -1,38 +1,35 @@
 <?php
 include '../header.php';
-include 'stylesheet.php';
 ?>
 
-<html lang="en" dir="ltr">
+  <html lang="en" dir="ltr">
+
   <head>
     <title>Opret bruger</title>
   </head>
 
   <div class="container-1">
     <div class="box-1">
-     <link href="style.css" rel="stylesheet" type="text/css">
-    <body>
-  <p style="font-family:quantify"><font size="7" color="white">Lokalebooking</font></p>
-    <!-- Creates all the labels for the register page -->
-    <!--<p class="sansserif" style="position:absolute;TOP:240;left:527"> Fornavn</P>
-    <p class="sansserif" style="position:absolute;TOP:265;left:517"> Efternavn</P>
-    <p class="sansserif" style="position:absolute;TOP:290;left:541"> Klasse</P>
-    <p class="sansserif" style="position:absolute;TOP:315;left:528"> Kodeord</P>
-    <p class="sansserif" style="position:absolute;TOP:340;left:473"> Bekræft Kodeord</P>-->
+      <link href="style.css" rel="stylesheet" type="text/css">
+
+      <body>
+        <p style="font-family:quantify">
+          <font size="7" color="white">Lokalebooking</font>
+        </p>
 
       </body>
     </div>
   </div>
 
-<div class="container-2">
-  <div class="box-2">
+  <div class="container-2">
+    <div class="box-2">
 
-  </div>
-  <div class="box-3">
-  <form name="form" action="Opret.php" method="POST">
-      <input type="text" placeholder="Navn" name="Navn" id="Navn"><br>
-      <input type="text" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
-      <select name="klasse" id="klasse" style="width:173px">
+    </div>
+    <div class="box-3">
+      <form name="form" action="Opret.php" method="POST">
+        <input type="text" placeholder="Navn" name="Navn" id="Navn"><br>
+        <input type="text" placeholder="Efternavn" name="Efternavn" id="Efternavn"><br>
+        <select name="klasse" id="klasse" style="width:173px">
       <option hidden="">Vælg klasse</option>
       <option value="16xaa">16xaa</option>
       <option value="16xab">16xab</option>
@@ -61,13 +58,13 @@ include 'stylesheet.php';
       <option value="18xaq">18xaq</option>
       <option value="18xar">18xar</option>
       </select>
-      <input type="password" placeholder="Kodeord" name="pw" id="pw"><br>
-      <input type="password" placeholder="Bekræft kodeord"name="cpw" id="cpw"><br>
+        <input type="password" placeholder="Kodeord" name="pw" id="pw"><br>
+        <input type="password" placeholder="Bekræft kodeord" name="cpw" id="cpw"><br>
 
-      <input HREF="../Front.php" type="submit" value="Opret bruger">
+        <input HREF="../Front.php" type="submit" value="Opret bruger">
       </form>
-  </div>
-  <div class="box-4">
+    </div>
+    <div class="box-4">
 
+    </div>
   </div>
-</div>
